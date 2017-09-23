c28a0c5c4be8c9e0f33e712867781d33b4be423e
Traci Williams <ug3000@gmail.com>
2017-09-23 18:07:36 -0500

    making view object

--- scripts/app2.js
+++ scripts/app2.js
@@ -2,7 +2,17 @@ window.onload = function() {
 	console.log("battleship loaded?")
 }
 
+var view = {
+	displayMessage: function (message) {
 
+	};
+	displayHit: function(location) {
+
+	};
+	displayMiss: function(location){
+
+	};
+}
 
 
 
