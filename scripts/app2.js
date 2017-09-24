613b77eb508e4c60bc80bd1d49d63e326d1ca9cc
Traci Williams <ug3000@gmail.com>
2017-09-23 20:08:42 -0500

    adding model object

--- scripts/app2.js
+++ scripts/app2.js
@@ -1,21 +1,38 @@
-window.onload = function() {
-	console.log("battleship loaded?")
-}
+//model object which is the state of the game
 
+
+//view object which updates the display
 var view = {
-	displayMessage: function (message) {
+	displayMessage: function(message) {
+		var messageArea = document.getElementById("messageArea");
+		messageArea.innerHTML = message;
+	},
 
-	};
 	displayHit: function(location) {
+		var cell = document.getElementById(location);
+		cell.setAttribute("class", "hit");
 
-	};
+	},
+
 	displayMiss: function(location){
+		var cell = document.getElementById(location);
+		cell.setAttribute("class", "miss");
 
-	};
-}
-
-
-
-
+	}
+};
 
 
+//to test view object methods
+/*view.displayMiss("00");
+view.displayHit("34");
+view.displayMiss("55");
+view.displayHit("12");
+view.displayMiss("25");
+view.displayHit("26");
+
+view.displayMessage("Is this working?");*/
+
+
+
+
+//controller object which handles the user's input
