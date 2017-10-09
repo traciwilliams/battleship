2f032d6c805bf92592f82494306f09e144d8cab0
Traci Williams <ug3000@gmail.com>
2017-10-09 18:42:17 -0500

    completed model object

--- scripts/app2.js
+++ scripts/app2.js
@@ -1,4 +1,28 @@
 //model object which is the state of the game
+/*numShip - number of ships in the game, shipsSunk - keeps the current number of ships that have been sunk by player
+shipLength - number of locations in each ship which is 3*/
+var model = {
+	boardSize: 7,
+	numShip: 3, 
+	shipLength: 3,
+	shipsSunk: 0,
+	ships: [{locations:["06","16","26"], hits:["","",""]},
+		   {locations:["24","34","44"], hits:["","",""]},
+		   {locations:["10","11","12"], hits:["","",""]}],
+	fire function(guess) {
+		for (var i = 0; i < this.numShip; i++) {
+			var ship = this.ships[i];
+			var locations = ship.locations;
+			var index = locations.indexOf(guess);
+			if (index >= 0) {
+				console.log("hit");
+			}
+		}
+	}	   
+
+};
+
+
 
 
 //view object which updates the display
@@ -27,10 +51,12 @@ var view = {
 view.displayHit("34");
 view.displayMiss("55");
 view.displayHit("12");
-view.displayMiss("25");
+view.displayMiss("25");	
 view.displayHit("26");
 
 view.displayMessage("Is this working?");*/
+
+
 
 
 
