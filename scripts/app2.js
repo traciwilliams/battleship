fa67e46ce7b2bfa2f4bd7b3b9765542a717a14d8
Traci Williams <ug3000@gmail.com>
2017-10-09 18:45:41 -0500

    completed model object

--- scripts/app2.js
+++ scripts/app2.js
@@ -16,8 +16,13 @@ var model = {
 			var index = locations.indexOf(guess);
 			if (index >= 0) {
 				console.log("hit");
+				ship.hits[index] = hit;
+				return true;
 			}
+			
 		}
+
+		return false;
 	}	   
 
 };
